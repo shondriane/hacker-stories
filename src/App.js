1d7f90b672a6cc348d24458a4ee8068b85eca9e6
shondriane <shondriane.wise@yahoo.com>
2022-03-21 08:15:52 -0400

    react components

--- src/App.js
+++ src/App.js
@@ -20,30 +20,41 @@ const list = [
 
 ];
 
-
+function List (){
+  return (
+    <ul>
+    {list.map(function (item){
+      return <li key= {item.objectID}> 
+      <span>
+        <a href ={item.url}>{item.title} </a>
+        </span>
+        <span> {item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        </li>;
+    })}
+  </ul>
+    
+    );
+}
+function Search (){
+ return(
+   <div>
+     <label htmlFor= "search"> Search: </label>
+ <input id ="search" type = "text" />
+ </div> 
+ );
+}
 function App() {
   return(
     <div>
       <h1> My Hacker Stories</h1>
 
-      <label htmlFor= "search"> Search: </label>
-      <input id ="search" type = "text" />
+     <Search/>
 
       <hr />
-      <ul>
-        {list.map(function (item){
-          return <li key= {item.objectID}> 
-          <span>
-            <a href ={item.url}>{item.title} </a>
-            </span>
-            <span> {item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>;
-        })}
-      </ul>
-
-      {/* render lists here*/}
+      
+     <List/>
     </div>
   );
 }
