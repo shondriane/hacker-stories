b0ddd8ad6c7eb3774a408f1c50b96f55057bf14d
shondriane <shondriane.wise@yahoo.com>
2022-04-24 14:01:27 -0400

    hooks and fragments

--- src/App.js
+++ src/App.js
@@ -1,64 +1,96 @@
-import * as React from 'react';
+import * as React from "react";
 
-const list = [
-  {
-    title:'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title:'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-
-];
-
-function List (){
+function List(props) {
   return (
-    <ul>
-    {list.map(function (item){
-      return <li key= {item.objectID}> 
-      <span>
-        <a href ={item.url}>{item.title} </a>
-        </span>
-        <span> {item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        </li>;
-    })}
-  </ul>
-    
-    );
-}
-function Search (){
- return(
-   <div>
-     <label htmlFor= "search"> Search: </label>
- <input id ="search" type = "text" />
- </div> 
- );
-}
-function App() {
-  return(
     <div>
-      <h1> My Hacker Stories</h1>
-
-     <Search/>
-
-      <hr />
-      
-     <List/>
+      <h2>{props.title}</h2>
+      <ul>
+        {props.list.map(function (item) {
+          return <Item item={item} />;
+        })}
+      </ul>
     </div>
   );
 }
+const Item = (props) => {
+  return (
+    <li key={props.item.objectID}>
+      <span>
+        <a href={props.item.url}>{props.item.title} </a>
+      </span>
+      <span> {props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
+function Search(props) {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    props.onSearch(event);
+  };
+
+  return (
+    <React.Fragment>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{props.searchTerm}</strong>
+      </p>
+    </React.Fragment>
+  );
+}
+const useStorageState =(key,initialState) =>{
+  const [value, setValue] = React.useState (
+     localStorage.getItem(key) || initialState
+   );
+   React.useEffect(()=> {
+     localStorage.setItem(key, value);
+   }, [value,key]);
+   return [value,setValue];
+ };
+function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+ 
+  const [searchTerm,setSearchTerm] = useStorageState ('search', 'React');
 
 
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+    /*localStorage.setItem('search', event.target.value);*/
+  };
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div>
+      <h1> My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
+
+      <hr />
+
+      <List list={searchedStories} title="React Ecosystem" />
+    </div>
+  );
+}
 
 export default App;
